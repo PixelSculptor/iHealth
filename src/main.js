ed8adc50a00a7fd19a6a47390f04aaed3f1d6c0c
Kacper Nadstoga <kapix007@gmail.com>
2022-06-01 17:26:45 +0200

    add variable for colors & typography

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './sass/main.scss'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  
+  
 `
 
