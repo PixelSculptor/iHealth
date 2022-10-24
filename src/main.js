5dadd6f47d5ff2eac6a6c0a7f620926d85a84b7b
Kacper Nadstoga <kapix007@gmail.com>
2022-10-24 22:28:16 +0200

    fix problem with disconnected routing

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue';
 import './style.scss';
 import App from './App.vue';
+import { createPinia } from 'pinia';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
-
+import router from './router/index.js';
 library.add(faUserSecret);
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .use(createPinia())
+    .use(router)
+    .mount('#app');
