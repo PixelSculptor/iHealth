5ff3a34c266c73d3405c4f99e1f4f1e29aa44abf
Julia <juliaglanc3@gmail.com>
2022-11-24 19:26:10 +0100

    Add firebase to our ihealth project (#8)

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBm6Y9sQlwOeCxyVqy4dSQ1nGBF61SJLbI',
+    authDomain: 'ihealth-engineer.firebaseapp.com',
+    projectId: 'ihealth-engineer',
+    storageBucket: 'ihealth-engineer.appspot.com',
+    messagingSenderId: '270633551876',
+    appId: '1:270633551876:web:eb3876f5a1a213d1818d51',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp };
