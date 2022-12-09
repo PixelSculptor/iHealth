2ba2d825d09690824658928dd78fca96c0db0da2
Julia Glanc <julia.glanc@wundermanthompson.com>
2022-12-09 18:24:40 +0100

    Add authentication in 3 methods: password and email, google, facebook

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: 'AIzaSyBm6Y9sQlwOeCxyVqy4dSQ1nGBF61SJLbI',
@@ -15,5 +16,6 @@ firebase.initializeApp(firebaseConfig);
 
 const projectFirestore = firebase.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+const projectAuth = firebase.auth();
 
-export { projectFirestore, timestamp };
+export { projectFirestore, timestamp, projectAuth };
