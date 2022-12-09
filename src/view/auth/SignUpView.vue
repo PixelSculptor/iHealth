d0168362acfa92c09c506777e53c2ef7196b4f60
Kacper Nadstoga <kapix007@gmail.com>
2022-12-09 23:35:08 +0100

    add brand header component and font awesome icons

--- src/view/auth/SignUpView.vue
+++ src/view/auth/SignUpView.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="wrapper">
+        <section class="welcome">
+            <section class="welcome-banner">
+                <brand-header-component />
+            </section>
+        </section>
         <singup-form-component />
     </div>
 </template>
 
 <script setup>
     import SingupFormComponent from '../../components/SingupFormComponent.vue';
+    import BrandHeaderComponent from '../../components/BrandHeaderComponent.vue';
 </script>
 
 <style scoped></style>
