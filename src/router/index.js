9aa06ea7af108d74eaac96f1f26ebcba51b2d32a
Kacper Nadstoga <52595886+Kapson007@users.noreply.github.com>
2022-12-10 19:12:49 +0100

    Sign up view (#11)
    * add RegisterView
    
    * add styles for headers & parahraph
    
    * add variables for designs
    
    * add mixins for input forms and buttons
    
    * create button component
    
    * add styles for SignupFormComponent
    
    * add brand header component and font awesome icons
    
    * create feature description component and add styles for banner subview
    
    * create registration panel and add styles for register form

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import WelcomeView from '../view/WelcomeView.vue';
+import SignUpView from '../view/auth/SignUpView.vue';
+import LoginView from '../view/auth/LoginView.vue';
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -7,6 +9,16 @@ const router = createRouter({
             path: '/',
             name: 'Welcome',
             component: WelcomeView,
+        },
+        {
+            path: '/register',
+            name: 'Signup',
+            component: SignUpView,
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: LoginView,
         },
     ],
 });
---
+++ src/view/auth/LoginView.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Login View</h1>
+</template>
+
+<script>
+    export default {
+        name: 'LoginView',
+    };
+</script>
+
+<style scoped></style>
