2d4b9ad4adde0dc921e2c6728b8e93953c9af9f5
Julia Glanc <julia.glanc@wundermanthompson.com>
2022-12-15 20:53:49 +0100

    Add dealing with collections, please check it if it correct

---
+++ src/composables/getCollections.js
@@ -0,0 +1,32 @@
+import { ref } from 'vue';
+import { projectFirestore } from '../firebase/config';
+
+const getCollection = (collection) => {
+    const documents = ref(null);
+    const error = ref(null);
+
+    const collectionRef = projectFirestore
+        .collection(collection)
+        .orderBy('createdAt');
+
+    collectionRef.onSnapshot(
+        (snap) => {
+            const results = [];
+            snap.docs.forEach((doc) => {
+                doc.data().createdAt &&
+                    results.push({ ...doc.data(), id: doc.id });
+            });
+            documents.value = results;
+            error.value = null;
+        },
+        (err) => {
+            console.log(err.message);
+            documents.value = null;
+            error.value = 'Could not fetch data, try again or search error';
+        }
+    );
+
+    return { documents, error };
+};
+
+export default getCollection;
