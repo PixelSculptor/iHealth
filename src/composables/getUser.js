d231ad022d426b4aebc60aa0db5845e9ed7297fd
Julia Glanc <julia.glanc@wundermanthompson.com>
2022-12-16 21:32:30 +0100

    resolve conflict

--- src/composables/getUser.js
+++ src/composables/getUser.js
@@ -1,16 +1,12 @@
-
 import { projectAuth } from '../firebase/config.js';
 import { ref } from 'vue';
 
 const user = ref(projectAuth.currentUser);
-
 projectAuth.onAuthStateChanged((_user) => {
     console.log(`Auth state has changed! current user is: `, _user);
     user.value = _user;
 });
-
 const getUser = () => {
     return { user };
 };
-
 export default getUser;
