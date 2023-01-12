0aa453929fbeff1b30b93376ed9083c763c94c21
Kacper Nadstoga <kapix007@gmail.com>
2023-01-12 14:46:05 +0100

    create logic for computing age and add whole design to profile overview sidebar and render userLabel component

--- src/components/profile/UserSidebarComponent.vue
+++ src/components/profile/UserSidebarComponent.vue
@@ -4,6 +4,9 @@
             <user-avatar-component />
             <h5>{{ name }} {{ surname }}</h5>
         </div>
+        <div class="userData">
+            <user-info-list-component />
+        </div>
     </div>
 </template>
 
@@ -12,6 +15,7 @@
     import useUserStore from '../../stores/userStore.js';
     import { storeToRefs } from 'pinia';
     import { ref } from 'vue';
+    import UserInfoListComponent from './UserInfoListComponent.vue';
 
     const userStore = useUserStore();
     const { getUserInfo } = storeToRefs(userStore);
@@ -25,15 +29,22 @@
         @include flex-position(column, nowrap, flex-start, center);
         height: 100vh;
         width: 100%;
+        padding: 1rem;
 
         .userAvatar {
             @include flex-position(column, nowrap, space-evenly, center);
             height: 30%;
+            width: 100%;
 
             h5 {
                 @include text-header5();
-                color: $gray-100;
+                color: $white;
             }
+        }
+
+        .userData {
+            width: 100%;
+            height: 70%;
         }
     }
 </style>
