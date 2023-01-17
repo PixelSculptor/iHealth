ec0b56f1c4ad59498dcd6d270aef5129589da7fe
Kacper Nadstoga <kapix007@gmail.com>
2023-01-17 23:07:13 +0100

    install V-Calendar lib

--- src/view/VisitsView.vue
+++ src/view/VisitsView.vue
@@ -1,10 +1,24 @@
 <template>
-    <h1>Visits view</h1>
+    <div class="">
+        <h1>Visits view</h1>
+        <v-date-picker
+            v-model="date"
+            color="indigo"
+            is-expanded
+            is-range />
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+    import { ref, watch } from 'vue';
 
-<style scoped lang="scss">
+    const date = ref(new Date());
+    watch(date, () => {
+        console.log(date.value);
+    });
+</script>
+
+<style lang="scss" scoped>
     * {
     }
 </style>
